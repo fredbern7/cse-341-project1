22c46492ebbfd0d2a56104c7a2df8f3e8b10081d
fredbern <fre19036@byui.edu>
2024-06-16 17:06:53 +1100

    init project1

---
+++ server.js
@@ -0,0 +1,10 @@
+// Express is used for web stuff
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes/index.js'));
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
