3cb54a315f0ce7044ca120d7c0ecc1f5512ae9b1
fredbern <fre19036@byui.edu>
2024-06-20 21:14:55 +1100

    update 3

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use((req, res, next) => {
 });
 app.use('/', require('./routes'));
 
-
-
 mongodb.initDb((err) => {
     if(err) {
         console.log(err);
--- swagger.js
+++ swagger.js
@@ -2,8 +2,8 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
-        title: 'Users Api',
-        description: 'Users Api'
+        title: 'Contact Api',
+        description: 'Contact Api'
     },
     host: 'localhost:3000',
     schemes: ['http', 'https']
